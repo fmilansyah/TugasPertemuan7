55b73342436e90f694e0cba2505a8219af1b8349
Febri Milansyah <febrimilan00@gmail.com>
2020-10-24 18:05:21 +0700

    For Next

--- TugasPertemuan7/Form1.Designer.vb
+++ TugasPertemuan7/Form1.Designer.vb
@@ -22,10 +22,200 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.btn_number_1 = New System.Windows.Forms.Button()
+        Me.list_number_1 = New System.Windows.Forms.ListBox()
+        Me.list_number_2 = New System.Windows.Forms.ListBox()
+        Me.btn_number_2 = New System.Windows.Forms.Button()
+        Me.list_number_3 = New System.Windows.Forms.ListBox()
+        Me.btn_number_3 = New System.Windows.Forms.Button()
+        Me.list_number_4 = New System.Windows.Forms.ListBox()
+        Me.btn_number_4 = New System.Windows.Forms.Button()
+        Me.list_number_5 = New System.Windows.Forms.ListBox()
+        Me.btn_number_5 = New System.Windows.Forms.Button()
+        Me.list_number_6 = New System.Windows.Forms.ListBox()
+        Me.btn_number_6 = New System.Windows.Forms.Button()
+        Me.list_number_7 = New System.Windows.Forms.ListBox()
+        Me.btn_number_7 = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.list_number_7)
+        Me.GroupBox1.Controls.Add(Me.btn_number_7)
+        Me.GroupBox1.Controls.Add(Me.list_number_6)
+        Me.GroupBox1.Controls.Add(Me.btn_number_6)
+        Me.GroupBox1.Controls.Add(Me.list_number_5)
+        Me.GroupBox1.Controls.Add(Me.btn_number_5)
+        Me.GroupBox1.Controls.Add(Me.list_number_4)
+        Me.GroupBox1.Controls.Add(Me.btn_number_4)
+        Me.GroupBox1.Controls.Add(Me.list_number_3)
+        Me.GroupBox1.Controls.Add(Me.btn_number_3)
+        Me.GroupBox1.Controls.Add(Me.list_number_2)
+        Me.GroupBox1.Controls.Add(Me.btn_number_2)
+        Me.GroupBox1.Controls.Add(Me.list_number_1)
+        Me.GroupBox1.Controls.Add(Me.btn_number_1)
+        Me.GroupBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox1.Location = New System.Drawing.Point(13, 13)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(892, 188)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "For ... Next"
+        '
+        'btn_number_1
+        '
+        Me.btn_number_1.Location = New System.Drawing.Point(7, 23)
+        Me.btn_number_1.Name = "btn_number_1"
+        Me.btn_number_1.Size = New System.Drawing.Size(120, 25)
+        Me.btn_number_1.TabIndex = 0
+        Me.btn_number_1.Text = "Angka 1"
+        Me.btn_number_1.UseVisualStyleBackColor = True
+        '
+        'list_number_1
+        '
+        Me.list_number_1.FormattingEnabled = True
+        Me.list_number_1.ItemHeight = 16
+        Me.list_number_1.Location = New System.Drawing.Point(7, 55)
+        Me.list_number_1.Name = "list_number_1"
+        Me.list_number_1.Size = New System.Drawing.Size(120, 116)
+        Me.list_number_1.TabIndex = 1
+        '
+        'list_number_2
+        '
+        Me.list_number_2.FormattingEnabled = True
+        Me.list_number_2.ItemHeight = 16
+        Me.list_number_2.Location = New System.Drawing.Point(133, 55)
+        Me.list_number_2.Name = "list_number_2"
+        Me.list_number_2.Size = New System.Drawing.Size(120, 116)
+        Me.list_number_2.TabIndex = 3
+        '
+        'btn_number_2
+        '
+        Me.btn_number_2.Location = New System.Drawing.Point(133, 23)
+        Me.btn_number_2.Name = "btn_number_2"
+        Me.btn_number_2.Size = New System.Drawing.Size(120, 25)
+        Me.btn_number_2.TabIndex = 2
+        Me.btn_number_2.Text = "Angka 2"
+        Me.btn_number_2.UseVisualStyleBackColor = True
+        '
+        'list_number_3
+        '
+        Me.list_number_3.FormattingEnabled = True
+        Me.list_number_3.ItemHeight = 16
+        Me.list_number_3.Location = New System.Drawing.Point(259, 54)
+        Me.list_number_3.Name = "list_number_3"
+        Me.list_number_3.Size = New System.Drawing.Size(120, 116)
+        Me.list_number_3.TabIndex = 5
+        '
+        'btn_number_3
+        '
+        Me.btn_number_3.Location = New System.Drawing.Point(259, 22)
+        Me.btn_number_3.Name = "btn_number_3"
+        Me.btn_number_3.Size = New System.Drawing.Size(120, 25)
+        Me.btn_number_3.TabIndex = 4
+        Me.btn_number_3.Text = "Angka 3"
+        Me.btn_number_3.UseVisualStyleBackColor = True
+        '
+        'list_number_4
+        '
+        Me.list_number_4.FormattingEnabled = True
+        Me.list_number_4.ItemHeight = 16
+        Me.list_number_4.Location = New System.Drawing.Point(385, 54)
+        Me.list_number_4.Name = "list_number_4"
+        Me.list_number_4.Size = New System.Drawing.Size(120, 116)
+        Me.list_number_4.TabIndex = 7
+        '
+        'btn_number_4
+        '
+        Me.btn_number_4.Location = New System.Drawing.Point(385, 22)
+        Me.btn_number_4.Name = "btn_number_4"
+        Me.btn_number_4.Size = New System.Drawing.Size(120, 25)
+        Me.btn_number_4.TabIndex = 6
+        Me.btn_number_4.Text = "Angka 4"
+        Me.btn_number_4.UseVisualStyleBackColor = True
+        '
+        'list_number_5
+        '
+        Me.list_number_5.FormattingEnabled = True
+        Me.list_number_5.ItemHeight = 16
+        Me.list_number_5.Location = New System.Drawing.Point(511, 54)
+        Me.list_number_5.Name = "list_number_5"
+        Me.list_number_5.Size = New System.Drawing.Size(120, 116)
+        Me.list_number_5.TabIndex = 9
+        '
+        'btn_number_5
+        '
+        Me.btn_number_5.Location = New System.Drawing.Point(511, 22)
+        Me.btn_number_5.Name = "btn_number_5"
+        Me.btn_number_5.Size = New System.Drawing.Size(120, 25)
+        Me.btn_number_5.TabIndex = 8
+        Me.btn_number_5.Text = "Angka 5"
+        Me.btn_number_5.UseVisualStyleBackColor = True
+        '
+        'list_number_6
+        '
+        Me.list_number_6.FormattingEnabled = True
+        Me.list_number_6.ItemHeight = 16
+        Me.list_number_6.Location = New System.Drawing.Point(637, 55)
+        Me.list_number_6.Name = "list_number_6"
+        Me.list_number_6.Size = New System.Drawing.Size(120, 116)
+        Me.list_number_6.TabIndex = 11
+        '
+        'btn_number_6
+        '
+        Me.btn_number_6.Location = New System.Drawing.Point(637, 23)
+        Me.btn_number_6.Name = "btn_number_6"
+        Me.btn_number_6.Size = New System.Drawing.Size(120, 25)
+        Me.btn_number_6.TabIndex = 10
+        Me.btn_number_6.Text = "Angka 6"
+        Me.btn_number_6.UseVisualStyleBackColor = True
+        '
+        'list_number_7
+        '
+        Me.list_number_7.FormattingEnabled = True
+        Me.list_number_7.ItemHeight = 16
+        Me.list_number_7.Location = New System.Drawing.Point(763, 54)
+        Me.list_number_7.Name = "list_number_7"
+        Me.list_number_7.Size = New System.Drawing.Size(120, 116)
+        Me.list_number_7.TabIndex = 13
+        '
+        'btn_number_7
+        '
+        Me.btn_number_7.Location = New System.Drawing.Point(763, 22)
+        Me.btn_number_7.Name = "btn_number_7"
+        Me.btn_number_7.Size = New System.Drawing.Size(120, 25)
+        Me.btn_number_7.TabIndex = 12
+        Me.btn_number_7.Text = "Angka 7"
+        Me.btn_number_7.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(917, 450)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents list_number_7 As ListBox
+    Friend WithEvents btn_number_7 As Button
+    Friend WithEvents list_number_6 As ListBox
+    Friend WithEvents btn_number_6 As Button
+    Friend WithEvents list_number_5 As ListBox
+    Friend WithEvents btn_number_5 As Button
+    Friend WithEvents list_number_4 As ListBox
+    Friend WithEvents btn_number_4 As Button
+    Friend WithEvents list_number_3 As ListBox
+    Friend WithEvents btn_number_3 As Button
+    Friend WithEvents list_number_2 As ListBox
+    Friend WithEvents btn_number_2 As Button
+    Friend WithEvents list_number_1 As ListBox
+    Friend WithEvents btn_number_1 As Button
 End Class
--- TugasPertemuan7/Form1.vb
+++ TugasPertemuan7/Form1.vb
@@ -1,3 +1,70 @@
 ﻿Public Class Form1
+    Private Sub btn_number_1_Click(sender As Object, e As EventArgs) Handles btn_number_1.Click
+        Dim i As Integer
 
+        For i = 1 To 4
+            list_number_1.Items.Add(i)
+        Next
+    End Sub
+
+    Private Sub btn_number_2_Click(sender As Object, e As EventArgs) Handles btn_number_2.Click
+        Dim i As Integer
+
+        For i = 1 To 4 Step 2
+            list_number_2.Items.Add(i)
+        Next
+    End Sub
+
+    Private Sub btn_number_3_Click(sender As Object, e As EventArgs) Handles btn_number_3.Click
+        Dim i As Integer
+
+        For i = 1 To 4
+            list_number_3.Items.Add(i + 1)
+        Next
+    End Sub
+
+    Private Sub btn_number_4_Click(sender As Object, e As EventArgs) Handles btn_number_4.Click
+        Dim i As Integer
+
+        For i = 1 To 4
+            list_number_4.Items.Add(i & " A")
+        Next
+    End Sub
+
+    Private Sub btn_number_5_Click(sender As Object, e As EventArgs) Handles btn_number_5.Click
+        Dim i As Integer
+        Dim a As String
+
+        For i = 1 To 4
+            If i Mod 2 = 0 Then
+                a = "Genap"
+            Else
+                a = "Ganjil"
+            End If
+
+            list_number_5.Items.Add(i & " " & a)
+        Next
+    End Sub
+
+    Private Sub btn_number_6_Click(sender As Object, e As EventArgs) Handles btn_number_6.Click
+        Dim i As Integer
+        Dim j As Integer
+
+        For i = 1 To 4
+            For j = 1 To i
+                list_number_6.Items.Add(i)
+            Next
+        Next
+    End Sub
+
+    Private Sub btn_number_7_Click(sender As Object, e As EventArgs) Handles btn_number_7.Click
+        Dim i As Integer
+        Dim j As Integer
+
+        For i = 1 To 4
+            For j = 1 To i
+                list_number_7.Items.Add(j & " " & i)
+            Next
+        Next
+    End Sub
 End Class
